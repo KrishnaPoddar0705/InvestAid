fbd23eeef7dd3690c5255a295c43e60cc11b6518
krishna poddar <krishnapoddar@krishnas-MacBook-Pro.local>
2020-09-18 01:31:41 -0400

    minor bug fixes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Plot from 'react-plotly.js';
 
 class App extends Component{
   constructor(props) {
-    super(props);
+    super(props);  
     this.state = {
       value: '',
       stocks1:[],
